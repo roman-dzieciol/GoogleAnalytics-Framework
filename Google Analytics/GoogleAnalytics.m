2c703acd57edce9a09bbe9364a8aa84f706c6921
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-05-24 20:00:04 +0200

    Add dummy source file

---
+++ Google Analytics/GoogleAnalytics.m
@@ -0,0 +1,12 @@
+//
+//  GoogleAnalytics.m
+//
+//  Copyright (c) 2015 Adrian Kashivskyy. All rights reserved.
+//
+//  Licensed under the MIT License.
+//
+
+#import "GAI.h"
+
+@interface GAI (Dummy) @end
+@implementation GAI (Dummy) @end
